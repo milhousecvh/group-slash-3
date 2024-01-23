dededa3d7d620dfe0e0b90602d9739f60166ec07
milhousecvh <135559562+milhousecvh@users.noreply.github.com>
2024-01-23 10:57:59 -0600

    starting mysql

--- src/main/java/com/slash3/travelapp/TravelAppApplication.java
+++ src/main/java/com/slash3/travelapp/TravelAppApplication.java
@@ -1,4 +1,4 @@
-package com.slash3.travelapp;
+ package com.slash3.travelapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
